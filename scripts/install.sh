004fbec55ab1e2fc2d7ac2864f8cc806ea4cf23b
Jeremy Thelen <thelen@gjuce.com>
2023-07-04 14:28:33 +0200

    change install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,19 +1,22 @@
+#!/bin/bash
+
 echo ""
 echo "               Symfony Starter Installer Script              "
 echo "This script requires sudo access. Please enter your password:"
 echo ""
 
 echo ""
-echo "Please enter a Projectname: "
+echo "Please enter a Project Name:"
 echo ""
 
-read -p '-> ' projectname
+# Prompt for project name
+read -r projectname
 
 echo ""
-echo "Updating System and install needed packages..."
+echo "Updating System and installing needed packages..."
 echo ""
 
-sudo apt update && apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 sudo apt install curl -y
 
 echo ""
@@ -25,35 +28,35 @@ sudo sh get-docker.sh &&
 sudo usermod -aG docker "$USER" &&
 sudo systemctl enable docker &&
 sudo systemctl start docker &&
-sudo apt install docker-compose-plugin -y
+sudo apt install docker-compose -y
 
 echo ""
 echo "Cloning Repository..."
 echo ""
 
-git clone https://github.com/RetteDasPlanetGit/SymfonyStarter.git $projectname &&
-cd $projectname
+git clone https://github.com/RetteDasPlanetGit/SymfonyStarter.git "$projectname" &&
+cd "$projectname"
 
 echo ""
-echo "Generate new APP_SECRET value..."
+echo "Generating new APP_SECRET value..."
 echo ""
 
 APP_SECRET=$(openssl rand -hex 32)
 sed -i "s|^APP_SECRET=.*$|APP_SECRET=$APP_SECRET|" .env
 
 echo ""
-echo "Build and start containers..."
+echo "Building and starting containers..."
 echo ""
 
-sudo docker compose build --build-arg APP_UID="$(id -u)" --build-arg APP_GID="$(id -g)" &&
-sudo docker compose up -d
+sudo docker-compose build --build-arg APP_UID="$(id -u)" --build-arg APP_GID="$(id -g)" &&
+sudo docker-compose up -d
 
 echo ""
-echo "Install Composer Packages..."
+echo "Installing Composer Packages..."
 echo ""
 
-sudo docker compose exec app composer install
+sudo docker-compose exec app composer install
 
 echo ""
-echo "Finished! You can reach your Symfony App on https://localhost"
+echo "Finished! You can reach your Symfony App at https://localhost"
 echo ""
